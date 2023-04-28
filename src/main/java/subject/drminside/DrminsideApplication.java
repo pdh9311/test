eb565248442481f1ea3c6cd8dc87c3b7b1b9455d
pdh9311 <padohy@gmail.com>
2023-04-29 07:25:00 +0900

    images

---
+++ src/main/java/subject/drminside/DrminsideApplication.java
@@ -0,0 +1,13 @@
+package subject.drminside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrminsideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrminsideApplication.class, args);
+    }
+
+}
---
+++ src/main/java/subject/drminside/LogoController.java
@@ -0,0 +1,2 @@
+package subject.drminside;
+
